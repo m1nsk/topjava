c6d45b7359c15190191949c59c8903a7ec5a49bc
m1nsk <punch.korolev@yandex.ru>
2018-03-10 00:30:34 +0300

    2.0 add fix patch

---
+++ src/main/java/ru/javawebinar/topjava/util/Person/Person.java
@@ -0,0 +1,19 @@
+package ru.javawebinar.topjava.util.Person;
+
+public class Person {
+    private final String name;
+    private final int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
