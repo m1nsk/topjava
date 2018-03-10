d6a2682fa69befa78f33f48cac9f50566ea8a248
m1nsk <punch.korolev@yandex.ru>
2018-03-10 16:34:28 +0300

    HW2 1-2-3-4 added "to.Meal" to git

---
+++ src/main/java/ru/javawebinar/topjava/to/Meal.java
@@ -0,0 +1,83 @@
+package ru.javawebinar.topjava.to;
+
+import ru.javawebinar.topjava.model.AbstractBaseEntity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Meal extends AbstractBaseEntity {
+    private Integer id;
+
+    private Integer userId;
+
+    private final LocalDateTime dateTime;
+
+    private final String description;
+
+    private final int calories;
+
+    public Meal(LocalDateTime dateTime, String description, int calories) {
+        this(null, null, dateTime, description, calories);
+    }
+
+    public Meal(Integer id, Integer userId, LocalDateTime dateTime, String description, int calories) {
+        super(id);
+        this.id = id;
+        this.userId = userId;
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public LocalDate getDate() {
+        return dateTime.toLocalDate();
+    }
+
+    public LocalTime getTime() {
+        return dateTime.toLocalTime();
+    }
+
+    public boolean isNew() {
+        return id == null;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "Meal{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", dateTime=" + dateTime +
+                ", description='" + description + '\'' +
+                ", calories=" + calories +
+                '}';
+    }
+}
