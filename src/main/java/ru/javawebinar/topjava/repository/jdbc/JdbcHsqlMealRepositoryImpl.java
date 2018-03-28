78095cbe7ebe84b7017f10a433bd4d3444a191e3
m1nsk <punch.korolev@yandex.ru>
2018-03-28 03:40:21 +0300

    HW5 base + optional started

---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcHsqlMealRepositoryImpl.java
@@ -0,0 +1,26 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Meal;
+
+import javax.sql.DataSource;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+@Profile({"hsqldb", "jdbc"})
+public class JdbcHsqlMealRepositoryImpl extends AbstractJdbcMealRepositoryImpl {
+    public JdbcHsqlMealRepositoryImpl(DataSource dataSource, JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(dataSource, jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
+    @Override
+    final Timestamp getTimestamp(LocalDateTime startDate) {
+        return Timestamp.valueOf(startDate);
+    }
+}
---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcPostgresMealRepositoryImpl.java
@@ -0,0 +1,23 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Repository
+@Profile({"hsqldb", "postgres"})
+public class JdbcPostgresMealRepositoryImpl extends AbstractJdbcMealRepositoryImpl {
+    public JdbcPostgresMealRepositoryImpl(DataSource dataSource, JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(dataSource, jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
+    @Override
+    final Timestamp getTimestamp(LocalDateTime startDate) {
+        return startDate;
+    }
+}
